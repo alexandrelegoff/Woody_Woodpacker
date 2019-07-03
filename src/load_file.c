db651d0c21fd5b2ae8626eed8a0846bac591bc4f
Alexandre Le goff <ale-goff@e1z2r5p31.42.us.org>
2019-07-02 21:55:54 -0700

    bug with load file

--- src/load_file.c
+++ src/load_file.c
@@ -6,7 +6,7 @@
 /*   By: ale-goff <ale-goff@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/30 20:29:12 by ale-goff          #+#    #+#             */
-/*   Updated: 2019/07/02 17:02:11 by ale-goff         ###   ########.fr       */
+/*   Updated: 2019/07/02 21:55:40 by ale-goff         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,23 @@ int			load_file(char *filename, t_file *file)
 {
 	int				fd;
 	struct stat		statbuf;
+	int				is_loader;
 
+	is_loader = ft_strcmp(filename, PAYLOAD) == 0 ? 1 : 0;
 	if ((fd = open(filename, O_RDONLY)) == -1)
 		return (ret_error(OPEN));
 	if ((fstat(fd, &statbuf)) == -1)
 		return (ret_error(STAT));
-	file->size = statbuf.st_size;
-	if ((file->ptr = mmap(0, file->size, PROT_READ, MAP_PRIVATE, fd, 0)) == MAP_FAILED)
+	if (is_loader)
+		file->size_loader = statbuf.st_size;
+	else
+		file->size_file = statbuf.st_size;
+	if (is_loader && (file->ptr_loader = mmap(0, file->size_loader,
+		PROT_READ, MAP_PRIVATE, fd, 0)) == MAP_FAILED)
 		return (ret_error(MMAP));
+	else if ((file->ptr_file = mmap(0, file->size_file,
+		PROT_READ, MAP_PRIVATE, fd, 0)) == MAP_FAILED)
+		return (ret_error(MMAP));
+	close(fd);
 	return (EXIT_SUCCESS);
 }
