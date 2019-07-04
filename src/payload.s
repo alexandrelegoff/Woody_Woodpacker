7367a2a66b22cdc8ead6baeab95744757790188f
Alexx42 <legoff.alexandre06@gmail.com>
2019-07-04 15:45:53 -0700

    new payload

--- src/payload.s
+++ src/payload.s
@@ -1,15 +1,25 @@
-section .text
-	global _main
-_main:
-		mov rax,0x2000004
-		mov rdi,1
-		lea rsi,[rel msg]
-		mov rdx, msg_end - msg
-		syscall
+global _start
 
-	mov rax,0x11111111
+_start:
+	push rax
+	push rdi
+	push rsi
+	push rdx
+
+	mov rax,1
+	mov rdi,1
+	lea rsi,[rel msg]
+	mov rdx,msg_end - msg
+	syscall
+
+	pop rdx
+	pop rsi
+	pop rdi
+	pop rax
+
+	mov rax,0x1111111111111111
 	jmp rax
 
 align 8
-	msg db '....WOODY.....',10,0
+	msg db "....WOODY....",10,0
 	msg_end db 0x0
