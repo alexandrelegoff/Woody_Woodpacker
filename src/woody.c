f805003634f63e3b749ce5af0fb6b0161bcd3eec
Alexandre Le goff <ale-goff@e1z2r5p31.42.us.org>
2019-07-03 19:24:24 -0700

    Solve problem with entry point

--- src/woody.c
+++ src/woody.c
@@ -6,7 +6,7 @@
 /*   By: ale-goff <ale-goff@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/30 20:08:31 by ale-goff          #+#    #+#             */
-/*   Updated: 2019/07/03 19:10:15 by ale-goff         ###   ########.fr       */
+/*   Updated: 2019/07/03 19:24:01 by ale-goff         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,8 @@ int				woody_woodpacker(char *filename)
 	if (woody.gap < 31)
 		return (ret_error(NO_SPACE));
 	memmove(file.ptr_file + woody.text_end, g_mov, 31);
-	elf64.ehdr->e_entry = (Elf64_Addr)(file.ptr_file + woody.text_end);
+	elf64.ehdr->e_entry = (Elf64_Addr)woody.text_segment->p_vaddr + woody.text_end;
+	printf("%llu\n", elf64.ehdr->e_entry);
 	write_file(&file, &elf64);
 	return (EXIT_SUCCESS);
 }
